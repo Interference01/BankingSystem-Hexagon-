34b0fe9c07715ec48dab21b1ac366c927add2d18
this4you <me_vlad@ukr.net>
2022-09-16 22:16:22 +0300

    Добавьте файлы проекта.

---
+++ BankingSystem[Hexagon]/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankingSystem_Hexagon_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
