e7bdb30431e607cea2bf2b4d21a49809e17cd9c0
Interference01 <valentin14sday@gmail.com>
2022-09-18 21:59:27 +0300

    Added extend repository

--- BankingSystem[Hexagon]/auth-module/core/models/User.cs
+++ BankingSystem[Hexagon]/auth-module/core/models/User.cs
@@ -7,12 +7,16 @@ using System.Threading.Tasks;
 namespace BankingSystem_Hexagon_.auth_module.models {
     internal class User {
         public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
         public Role _Role { get; set; }
         public enum Role { Client, Admin }
 
         public User(Guid id, Role role) {
             Id = id;
             _Role = role;
+            Login = "user";
+            Password = "1111";
         }
     }
 }
---
+++ BankingSystem[Hexagon]/auth-module/repositories/ExtendRepository.cs
@@ -0,0 +1,18 @@
+﻿using BankingSystem_Hexagon_.auth_module.models;
+
+namespace BankingSystem_Hexagon_.auth_module.repositories {
+    static class ExtendRepository {
+
+        public static List<User> Users = new List<User>() {
+                new User(Guid.NewGuid(), User.Role.Client) {Login = "Lav"},
+                new User(Guid.NewGuid(), User.Role.Client){Login = "Val" },
+                new User(Guid.NewGuid(), User.Role.Client){Login = "Sara" },
+                new User(Guid.NewGuid(), User.Role.Admin){Login = "Vlad" }
+        };
+
+
+
+
+
+    }
+}
