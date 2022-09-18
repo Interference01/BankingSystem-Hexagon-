d99b404fadacd2fbd92ea6fd58969be237c1aef1
Interference01 <valentin14sday@gmail.com>
2022-09-18 22:09:32 +0300

    Aded some details

--- BankingSystem[Hexagon]/auth-module/repositories/ExtendRepository.cs
+++ BankingSystem[Hexagon]/auth-module/repositories/ExtendRepository.cs
@@ -1,14 +1,24 @@
-﻿using BankingSystem_Hexagon_.auth_module.models;
+﻿using BankingSystem_Hexagon_.auth_module.core.ports;
+using BankingSystem_Hexagon_.auth_module.models;
 
 namespace BankingSystem_Hexagon_.auth_module.repositories {
-    static class ExtendRepository {
+    class ExtendRepository : IAuthRepository {
 
-        public static List<User> Users = new List<User>() {
+        public List<User> Users = new List<User>() {
                 new User(Guid.NewGuid(), User.Role.Client) {Login = "Lav"},
                 new User(Guid.NewGuid(), User.Role.Client){Login = "Val" },
                 new User(Guid.NewGuid(), User.Role.Client){Login = "Sara" },
                 new User(Guid.NewGuid(), User.Role.Admin){Login = "Vlad" }
         };
+
+        public User? GetUser(string login, string password) {
+            User user= Users.Find(x => x.Login == login && x.Password == password);
+
+            if (user != null) 
+            return user;
+
+            return null;
+        }
 
 
 
