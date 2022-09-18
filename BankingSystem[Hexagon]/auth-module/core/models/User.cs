f33fb2143f2c31ca579e678a17da58ef408a3095
Interference01 <valentin14sday@gmail.com>
2022-09-18 23:26:00 +0300

    Added repository list

--- BankingSystem[Hexagon]/auth-module/core/models/User.cs
+++ BankingSystem[Hexagon]/auth-module/core/models/User.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace BankingSystem_Hexagon_.auth_module.models {
     internal class User {
         public Guid Id { get; set; }
         public Role _Role { get; set; }
-        public enum Role { Client, Admin }
 
         public User(Guid id, Role role) {
             Id = id;
             _Role = role;
         }
+
+        public enum Role { Client, Admin }
     }
 }
