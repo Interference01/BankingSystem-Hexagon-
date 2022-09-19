cc421c3db4c44e41f260ba74e984be36397c382c
Interference01 <89568577+Interference01@users.noreply.github.com>
2022-09-19 17:02:39 +0300

    added use case and register struct

---
+++ BankingSystem[Hexagon]/admin-module/core/ports/IRegisterRepository.cs
@@ -0,0 +1,15 @@
+﻿using BankingSystem_Hexagon_.admin_module.core.models;
+using BankingSystem_Hexagon_.auth_module.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem_Hexagon_.admin_module.core.ports
+{
+    internal interface IRegisterRepository
+    {
+        public Client? SaveClient(string name, string surname, string email, string password, string phone);
+    }
+}
---
+++ BankingSystem[Hexagon]/admin-module/core/use-cases/RegisterClientUseCase.cs
@@ -0,0 +1,33 @@
+﻿using BankingSystem_Hexagon_.admin_module.core.models;
+using BankingSystem_Hexagon_.admin_module.core.ports;
+using BankingSystem_Hexagon_.auth_module.models;
+using System.Numerics;
+using System.Xml.Linq;
+
+namespace BankingSystem_Hexagon_.admin_module.core.use_cases
+{
+    internal class RegisterClientUseCase
+    {
+        private readonly IRegisterRepository registerRepository;
+        public RegisterClientUseCase(IRegisterRepository registerRepository)
+        {
+            this.registerRepository = registerRepository;
+        }
+        public Client Register(string name, string surname, string email, string password, string phone)
+        {
+            //valid
+            if (name.Length == 20 && surname.Length == 20 && email.Length == 20 && password.Length == 20 && phone.Length == 20)
+            {
+                throw new Exception ("Max length it is 20");
+            }
+            if (true)
+            {
+
+            }
+
+            Client client = registerRepository.SaveClient( name,  surname,  email,  password,  phone);
+            return client;
+        }
+
+    }
+}
