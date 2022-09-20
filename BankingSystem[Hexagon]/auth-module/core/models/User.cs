8b14e984ddebf2b68d44940fc9ceda7b2458aa09
Interference01 <valentin14sday@gmail.com>
2022-09-20 22:55:11 +0300

    Added test for auth logic

--- BankingSystem[Hexagon]/auth-module/core/models/User.cs
+++ BankingSystem[Hexagon]/auth-module/core/models/User.cs
@@ -1,6 +1,6 @@
 ﻿
 namespace BankingSystem_Hexagon_.auth_module.models {
-    internal class User {
+    public class User {
         public Guid Id { get; set; }
         public Role _Role { get; set; }
 
