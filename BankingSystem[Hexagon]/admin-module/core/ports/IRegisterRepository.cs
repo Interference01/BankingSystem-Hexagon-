1542bdd9d5a141a02af44ad69873bd85c4072165
Interference01 <89568577+Interference01@users.noreply.github.com>
2022-09-22 21:03:04 +0300

    BS-5: Implement admin-module logic | Register (#8)
    * added struct folder
    
    * added use case and register struct
    
    * adde registr validate logic
    
    * added FieldValidationLib
    
    * updated master changes
    
    * added test for registr logic
    
    * added some changes for registr test
    
    * fix

---
+++ BankingSystem[Hexagon]/admin-module/core/ports/IRegisterRepository.cs
@@ -0,0 +1,15 @@
+﻿using BankingSystem_Hexagon_.admin_module.core.models;
+using BankingSystem_Hexagon_.auth_module.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem_Hexagon_.admin_module.core.ports
+{
+    public interface IRegisterRepository
+    {
+        public void SaveClient(Client client);
+    }
+}
---
+++ BankingSystem[Hexagon]/admin-module/core/use-cases/RegisterClientUseCase.cs
@@ -0,0 +1,23 @@
+﻿using BankingSystem_Hexagon_.admin_module.core.exception;
+using BankingSystem_Hexagon_.admin_module.core.models;
+using BankingSystem_Hexagon_.admin_module.core.ports;
+using FieldValidationLib;
+
+namespace BankingSystem_Hexagon_.admin_module.core.use_cases {
+    public class RegisterClientUseCase {
+        private readonly IRegisterRepository registerRepository;
+        public RegisterClientUseCase(IRegisterRepository registerRepository) {
+            this.registerRepository = registerRepository;
+        }
+        public void Register(Client client) {
+
+            FieldValidation.LengthValidate("Name", client.Name, 20);
+            FieldValidation.LengthValidate("Surname", client.Surname, 20);
+            FieldValidation.LengthValidate("Phone", client.Phone, 14);
+            FieldValidation.PasswordValidate(client.Password);
+            FieldValidation.EmailValidate(client.Email);
+
+            registerRepository.SaveClient(client);
+        }
+    }
+}
